53807c3345945aa9256d4d776c85d007abf2a8b0
Cory Koehler <cjiek9181@gmail.com>
2020-07-08 21:35:55 -0400

    Commit july 7th exercises

---
+++ ExampleLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleLibrary
+{
+    class Class1
+    {
+        something wrong and not c#
+    }
+}
---
+++ July7Examples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace July7Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            var userInput = Console.ReadLine();
+            Console.WriteLine("enter another number");
+            var secondUserInput = Console.ReadLine();
+
+            var isDecimal = decimal.TryParse(userInput, out decimal userInputAsDecimal);
+            var isSecondInputDecimal = decimal.TryParse(secondUserInput, out decimal secondUserInputAsDecimal);
+            
+            if (!isDecimal || !isSecondInputDecimal)
+            {
+                Console.WriteLine("Error - incorrect input - Try again");
+                return;
+            }
+
+            var sum = userInputAsDecimal + secondUserInputAsDecimal;
+            Console.WriteLine($"{userInputAsDecimal} + {secondUserInputAsDecimal}");
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
