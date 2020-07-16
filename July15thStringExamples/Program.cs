c01c42ecb45a49179bd42f99bce0bfe3e4ce3d88
Cory Koehler <cjiek9181@gmail.com>
2020-07-16 18:35:34 -0400

    Fix folder structure from july 15th examples

---
+++ July15thStringExamples/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace July15thStringExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Concatenation
+            Console.WriteLine("HI"+"I'M"+"TAYLORE");
+
+            var somethingFunky = string.Concat("hi", "bye");
+            Console.WriteLine(somethingFunky);
+
+            Console.WriteLine($"Here is {somethingFunky}");
+
+            //Split
+            var sidSpeaks = "Sid,has a nice voice,great.";
+            var sidSpeaksSplit = sidSpeaks.Split(',');
+
+            //[sid, has a nice voice, great]
+            // 0  , 1               , 2
+            for (int i = 0; i < sidSpeaksSplit.Length; i++)
+            {
+               Console.WriteLine($"Index{i}:{sidSpeaksSplit[i]}");
+            }
+
+            // Whitespace example
+            string whitespace = "       hello   world                ";
+            Console.WriteLine(whitespace.Trim());
+
+            //name = name.Replace(" ", "");
+            Console.WriteLine(whitespace.Replace("  ", " "));
+
+            //String
+            var multiTeacher = "Kori my name Elizabeth";
+            var FirstIndex = multiTeacher.IndexOf("Elizabeth");
+            var SecondIndex = multiTeacher.ToLower().IndexOf("beth", StringComparison.CurrentCultureIgnoreCase);
+            Console.WriteLine(FirstIndex);
+            Console.WriteLine(SecondIndex);
+
+            //String Comparison
+            var userInput = Console.ReadLine();
+            if (userInput.Equals("KORI",StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine("HEY IT DOES EQUAL!!");
+            }
+        }
+    }
+}
