a7c5802d782db93bd9d41bbaa20765237e2e1476
Cory Koehler <cjiek9181@gmail.com>
2020-07-20 22:25:32 -0400

    Add July 20th test example -> did not finish class content for testing

---
+++ July20thTestingExamples/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace July20thTestingExamples
+{
+    public class Class1
+    {
+        [Test]
+        public void OneEqualsOne()
+        {
+            var expected = 1;
+            var actual = 1;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
