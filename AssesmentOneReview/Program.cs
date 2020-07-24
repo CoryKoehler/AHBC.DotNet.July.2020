0a12b8557d89f76657b157ab3d22a3c8a80c97ff
Darion Thompson <darionthompson@Darions-MacBook-Pro.local>
2020-07-23 22:03:02 -0400

    commits for both assessment review and arrays

---
+++ AssesmentOneReview/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AssesmentOneReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var something = IsTheSame(6,8);
+            var somethingElse = Subtract(6,8);
+            var somethingnew = FindBuildingType(20);
+            Console.WriteLine(something);
+            Console.WriteLine(somethingElse);
+            Console.WriteLine(somethingnew);
+     
+        }
+
+           
+        
+        static bool IsTheSame(int numberOne, int numberTwo)
+        {
+            return numberOne == numberTwo;
+        }
+
+        static double Subtract(double num1, double num2)
+        {
+            return num2 - num1;
+        }
+
+        static string FindBuildingType(int num1)
+        {
+            if (num1 >= 4 && num1 <= 10)
+            {
+
+                return $"This is an office building {num1}";
+            }
+            else if (num1 > 50)
+            {
+                return " This is a Super skyscraper";
+
+            }
+            else if (num1 >= 11 && num1 <= 49)
+            {
+                return "This is a skyscraper!";
+            }
+            else if (num1 <= 3 && num1 >=0)
+            {
+                return "This is a house";
+            }
+            else if (num1 < 0)
+            {
+                return "Underground Bunker!";
+            }
+            return "Somthing";
+
+        }
+    }
+}
---
+++ July23rdExamples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace July23rdExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array
+            int[] arrayOne = new int[4];
+            arrayOne[0] = 1;
+            arrayOne[1] = 2;
+            arrayOne[2] = 3;
+            arrayOne[3] = 4;
+
+            int[] arrayTwo = new int[] { 1, 2, 3, 4, 4, 5, 6 };
+
+            char[] charArray = { 'D', 'E'};
+
+            int[] arrayThree = arrayTwo;
+            arrayThree[1] = 100;
+
+            var nestedArrays = GetNestedArray();
+
+            PrintNestedArrays(nestedArrays);
+
+        }
+
+        private static void PrintNestedArrays(int[][] nestedArrays)
+        {
+            for (int i = 0; i < nestedArrays.Length; i++)
+            {
+                Console.WriteLine($"OuterArrray{nestedArrays[i]}");
+
+                for (int j = 0; j < nestedArrays[i].Length; j++)
+                {
+                    Console.WriteLine($"InnerArray :{nestedArrays[j][i]}");
+                }
+            }
+        }
+
+        private static int[][] GetNestedArray()
+        {
+            int[][] nestedArray = new[]
+            {
+                       //J 0,1,2,3,4
+                new int[]{1,2,3,4,5},         //i 0
+                new int[]{6,7,8,9 },           //1
+                new int[]{10,11,12,13,14,15 }   //2
+            };
+            return nestedArray;
+        }
+    }
+}
