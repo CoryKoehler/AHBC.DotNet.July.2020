0fb39672eee73aed329b9c3e1f9a0a6abd59384b
Darion Thompson <darionthompson2@gmail.com>
2020-08-10 21:19:11 -0400

    examples from file I/O

---
+++ August10thI.OExamples/FileUtility.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace August10thI.OExamples
+{
+    public class FileUtility
+    {
+        public static void CreateFile(string fileName)
+        {
+            string filePath = @"C:\Darion\Desktop";
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+            }
+        }
+
+        public static void ModifyFile(string filename, bool canAppend, List<string> linesOfinput = null)
+        {
+            using (StreamWriter writer = new StreamWriter(filename, canAppend))
+            {
+                if(linesOfinput == null)
+                {
+                    writer.WriteLine("This is a new string");
+                }
+                else
+                {
+                    foreach (var line in linesOfinput)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        public static void ReadFromfile(string fileName)
+        {
+            using(StreamReader reader = new StreamReader(fileName))
+            {
+                //This is the old way of doing a read
+                //while (true)
+                //{
+                //    var line = reader.ReadLine();
+                //    if (line == null)
+                //    {
+                //        break;
+                //    }
+                //    Console.WriteLine(line);
+                //}
+
+                //
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+            }
+        }
+
+        public static void DeleteFile(string fileName)
+        {
+            File.Delete(fileName);
+        }
+    }
+}
---
+++ August10thI.OExamples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace August10thI.OExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "DarionsFile";
+            //Create
+            FileUtility.CreateFile(fileName);
+            //Inital Write
+            FileUtility.ModifyFile(fileName, false);
+            //Read
+            FileUtility.ReadFromfile(fileName);
+            var linesOfInput = new List<string>
+            {
+                "Darion Like Basketball",
+                "Star wars is a gift to makind",
+                "I named ,y son Anikan"
+            };
+
+            //Modify
+            FileUtility.ModifyFile(fileName, true, linesOfInput);
+            FileUtility.DeleteFile(fileName);
+        }
+    }
+}
